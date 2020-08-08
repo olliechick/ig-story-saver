24d82037526707aa40570a248cb384f5f2d4c1bd
Ollie Chick <och26@uclive.ac.nz>
2020-08-08 13:54:49 +1200

    Allow using custom stories directory in fix_dates

--- fix_dates.py
+++ fix_dates.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 import datetime
 import os
+import sys
 
 from ig_story_saver import STORIES_DIR, set_date, DATETIME_FORMAT
 
-directories = os.listdir(STORIES_DIR)
+stories_dir = STORIES_DIR
+
+if len(sys.argv) > 1:
+    print("Fixing stories in " + sys.argv[1])
+    stories_dir = sys.argv[1]
+
+directories = os.listdir(stories_dir)
 
 for directory in directories:
-    for filename in os.listdir(os.path.join(STORIES_DIR, directory)):
+    for filename in os.listdir(os.path.join(stories_dir, directory)):
         date_string = filename
         if '(' in date_string:
             parts = date_string.split(' ')
@@ -22,4 +29,4 @@ for directory in directories:
         date_string = date_string.replace('-', '_').replace("am", "AM").replace("pm", "PM")
         timestamp = datetime.datetime.strptime(date_string, DATETIME_FORMAT).timestamp()
 
-        set_date(os.path.join(STORIES_DIR, directory, filename), timestamp)
+        set_date(os.path.join(stories_dir, directory, filename), timestamp)
